49f7691acdd7ca441270ce44159716ad364ab4fb
Ivancho894 <serralta.ivan@gmail.com>
2023-04-07 12:44:25 -0300

    12

---
+++ Eje12.c
@@ -0,0 +1,16 @@
+#include <stdio.j>
+
+
+int main(){
+    int a;
+    int b;
+    int c;
+    printf("Ingrese el primer numero: ");
+    scanf("%d",&a)
+    printf("Ingrese el segundo numero: ");
+    scanf("%d",&b);
+    printf("Ingrese el tercer numero: ");
+    scanf("%d",&c);
+    int c = a+b+c;
+    printf("El resultado es: %d",c)
+}
