c581ec946cff4882689fafa0c3c292dbda206240
Ivancho894 <serralta.ivan@gmail.com>
2023-04-27 17:37:36 -0300

    Ejercicios parcial

--- Eje22.c
+++ Eje22.c
@@ -2,13 +2,14 @@
 
 int main(){
     int a,i;
+    int cont = 0;
     for (i = 0; i<5;i++){
     printf("Ingresa un nuemero: ");
     scanf("%d",&a);
     if (a%3!=0){
-        printf("El numero %d es no multiplo de 3 \n",a);
+        printf("El numero %d es no multiplo de 3 \n",a); nn
     }else {
-        printf("El numero %d es multiplo de 3\n",a);
+        cont ++;
     }
     }
     return 0;
