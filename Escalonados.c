cb64c76b7caee0d13616dfcbd6861812bacc17a2
Ivan Serralta <serralta.ivan@gmail.com>
2023-05-05 09:39:32 -0300

    Parcial mas printf

--- Escalonados.c
+++ Escalonados.c
@@ -9,11 +9,14 @@ int main (){
     b=num%10;
     if (a<b){
         cont+=b-a;
+        printf("a= %d b= %d cont= %d \n",a,cont);
     }
     a--;
     while (a!=0){
         cont+=9-a;
         a--;
+        printf("a= %d cont= %d\n",a,cont);
+
     }
     printf("Hay %d numeros escalonados antes de %d",cont,num);
     return 0;
