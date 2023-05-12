5e75e89c109d6fe75bc40ae2e2cbb529c23a79be
Ivan Serralta <serralta.ivan@gmail.com>
2023-05-12 09:40:23 -0300

    Arrays

---
+++ FirstArr.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main(){
+
+    int arr[4];
+    int i;
+    for (i=0; i<4;i++){
+        arr[i]=i*45;
+    }
+    for (i=0;i<4;i++){
+
+    printf("%d\n",arr[i]);
+    }
+}
