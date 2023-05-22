07789fd91b3310afa674cae8cfb02fdbfadab2eb
Ivan <serralta.ivan@gmail.com>
2023-05-22 20:20:55 -0300

    arreglando

--- Eje36.c
+++ Eje36.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+// #include <ctype.h>
 #include <string.h>
-#define TAM 4
+#define T 4 
 
 
 int main(){
     char a;
-    char elementos[4];
+    char elementos[T];
 
-    for(int i=0; i<4;i++){
+    for(int i=0; i<T;i++){
+        fflush(stdin);
         printf("Ingrese una letra:");
         elementos[i] = getchar();
-        a = getchar();
-        //scanf("%c",&elementos[i]);
     }
 
-    printf("Los caracteres ingresados son: %c",elementos[0]);
-    for(int j = 1; j<4 ;j++){
-        putchar(elementos[j]);
-        //printf("%c",elementos[j]);
+    
+    for(int j = 1; j<T-1 ;j++){
+        printf("%c, ",elementos[j]);
     }
+    printf("%c.\n",elementos[T]);
     return 0;
 }
