690e977e21378fca2cd13c44cf21dc74b4355a21
Ivan Serralta <serralta.ivan@gmail.com>
2023-08-09 19:11:59 -0300

    g

---
+++ EjeParcialPipe.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include T
+
+int main(){
+    int nroSocio[T],nacimiento[T],deporte[T];
+
+    for (int j = 0;j<T;j++){
+        deporte[j]=0;
+    }
+    for (int i = 0; i<T;i++){
+        printf("Ingrese el numero de socio del socio numero %d",i+1);
+        scanf("%d",&nroSocio[i]);
+        printf("Ingrese el deporte del socio numero %d",i+1);
+        scanf("%d",&deporte[i]);
+        while ()
+        printf("Ingrese el nacimiento del socio numero %d",i+1);
+        scanf("%d",&nacimiento[i]);
+        
+    }
+
+
+}
--- Parcial2022.c
+++ Parcial2022.c
@@ -4,11 +4,8 @@
 #include <string.h>
 
 int main(){
-    char aCargar[10];
-    // int aCargarNum[10];
     char nada;
-    int cont = 0;
-    // int m=0;
+    int m=0;
     int deudas[100];
     char cuotas[100];
     int j=-1;
@@ -22,8 +19,6 @@ int main(){
         //Escaneo deuda
         while (deudas[i]<=0){
         printf("Ingrese el valor de las deuda del cliente numero %d: ",i+1);
-        // gets(aCargarNum);
-        // deudas[i]=aCargarNum[0];
         scanf("%d",&deudas[i]);
         scanf("%c",&nada);
         }
@@ -32,40 +27,23 @@ int main(){
         while (cuotas[i]!= 'B' && cuotas[i]!= 'P' && cuotas[i]!= 'S'){
 
         printf("Ingrese el plan de financiacion del cliente numero %d: ",i+1);
-        gets(aCargar);
-        cuotas[i]=aCargar[0];
-        // scanf("%c",&cuotas[i]);
-        // scanf("%c",&nada);
+        scanf("%c",&cuotas[i]);
+        scanf("%c",&nada);
 
         }
 
         printf("Ingrese 1 para continuar y 0 para finalizar la carga: ");
-        // gets(aCargarNum);
-        // j=aCargarNum[0];
         scanf("%d",&j);
         scanf("%c",&nada);
         
-        i=i+1;
+        i++;
     }while (j==1);
     
     printf("\n\nEstos son los datos de los clientes cargados: \n");
     printf("Clente nro | Deuda | plan de financiacion\n");
     for (int k = 0; k< i ;k++){
-        printf("    %d      |  %c    |      %d\n",k+1,cuotas[k],deudas[k]);
-        if (deudas[k]>= 10000){
-            cont++;
-        }
+        printf("    %d      |  %d  |      %c\n",k+1,deudas[m],cuotas[m]);
     }
-    printf("\n\nLa cantidad de deudores que poseen una deuda mayor a 10000 es: %d.\n\n\n",cont);
     
-    printf("Los siguentes clientes accediron al plan social:\n\n");
-    printf("Clente nro | Cuota\n");
-    
-    for (int o = 0; o < i; o++){
-
-        if (cuotas[o] == 'S'){
-            printf("%d       |   %d\n",o+1,deudas[o]/24);
-        }
-    }
 
 }
