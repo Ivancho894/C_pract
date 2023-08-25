d65bac4f9429894b8548bd1b134cbfdb02363317
Ivan <serralta.ivan@gmail.com>
2023-08-25 15:58:13 -0300

    fee

--- Second/Tabla de pitagoras.c
+++ Second/Tabla de pitagoras.c
@@ -5,34 +5,33 @@ int defTam(){
     int tam = 0;
     while (tam<=0 || tam > 12){
     printf("Ingrese el tamano de la tabla de pitagoras (entre 1 y 12): ");
-    scanf("%d",tam);
+    scanf("%d",&tam);
     }
     return tam;
 }
 
-int calcular(int tam){
+void calcular(int tam){
+    
     int matriz[tam][tam];
-    for (int p = 0; p<=tam; p++){
-        for (int m = 0;m<=tam;m++){
-            matriz[p][m]=p*m;
+    for (int p = 0; p<tam; p++){
+        for (int m = 0;m<tam;m++){
+            matriz[p][m]=(p+1)*(m+1);
         }
     }
-    return matriz;
-}
-int mostrar(int matriz[][],int tam){    
-    for (int i = 1;i<=tam;i++){
+
+    for (int i = 0;i<tam;i++){
         printf("\n");
-        for (int j = 1;j<=tam;j++){
-            printf("%d   ",matriz[i][j]);
+        for (int j = 0;j<tam;j++){
+            printf("%3d\t",matriz[i][j]);
         }
     }
-    return 0;
+    return ;
 }
     
 
 int main(){
     int tam = defTam();
     
-    mostrar(calcular(tam),tam);
+    calcular(tam);
 
-}      
+}       
